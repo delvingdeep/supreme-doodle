45c9844c085aa020dd9c88b359469d4b5d447cfc
lohitarani <lchamart@mtu.edu>
2017-01-30 21:52:16 -0500

    Updating python code with proper timing functions. This version works well with just one launch file consisting gazebo and this code

--- project_three_ws/src/random_move.py
+++ project_three_ws/src/random_move.py
@@ -8,23 +8,24 @@ import time
 
 if __name__ == "__main__":
     try:
-    	rospy.init_node("random_move")
-    	rate = rospy.Rate(0.08)
-	init_time = rospy.Time.now()
+    	rospy.init_node("test_random")
+    	rate = rospy.Rate(0.033)
+	init_time = rospy.get_time()
 	seq=0
-    	while not rospy.is_shutdown() and rospy.Time.now().secs-init_time.secs<600:
+    	while not rospy.is_shutdown() :
     		pub = rospy.Publisher("/move_base/goal", MoveBaseActionGoal, queue_size=10)
 		cord=MoveBaseActionGoal()
-		seq=seq+1
 		cord.header.seq=seq
 		cord.header.stamp=rospy.Time.now()
 		cord.goal.target_pose.header.seq=seq
 		cord.goal.target_pose.header.stamp=rospy.Time.now()
 		cord.goal.target_pose.header.frame_id="map"
-		cord.goal.target_pose.pose.position.x=-10
-		cord.goal.target_pose.pose.position.y=-10
-
+		cord.goal.target_pose.pose.position.x=random.uniform(-10,10)
+		cord.goal.target_pose.pose.position.y=random.uniform(-10,10)
+		cord.goal.target_pose.pose.orientation.w=1.0
+		#print cord
 		pub.publish(cord)
+		seq=seq+1
 	   	rate.sleep()
 	    
     except rospy.ROSInterruptException:
